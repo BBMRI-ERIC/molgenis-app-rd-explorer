899a809d2a234c4373d2cd310cd5285da30f5fc0
Pacific89 <philipp.faulhammer@medunigraz.at>
2020-11-23 14:32:48 +0100

    fix getters

--- src/store/getters.js
+++ src/store/getters.js
@@ -4,90 +4,92 @@ import filterDefinitions from '../utils/filterDefinitions'
 
 export default {
 
-  filterDefinitions,
-  bookmarkMappedToState: state => state.bookmarkMappedToState,
-  loading: ({ collectionInfo, biobankIds }) => !(biobankIds && collectionInfo),
-  biobanks: ({ collectionInfo, biobankIds, biobanks }, { loading, rsql }) => {
-    if (loading) {
-      return []
-    }
-    let ids = biobankIds
-    if (rsql && rsql.length) {
-      ids = collectionInfo
-      // biobank IDs present in collectionIds
-        .map(({ biobankId }) => biobankId)
-      // also present in biobankIds
-        .filter(biobankId => biobankIds.includes(biobankId))
-      // first occurrence of ID only
-        .filter((value, index, self) => self.indexOf(value) === index)
-    }
-    return ids.map(biobankId => {
-      if (!Object.prototype.hasOwnProperty.call(biobanks, biobankId)) {
-        return biobankId
-      }
-      const biobank = biobanks[biobankId]
-      return {
-        ...biobank,
-        collections: filterCollectionTree(collectionInfo.map(it => it.collectionId), biobank.collections)
-      }
-    })
-  },
-  getFoundBiobankIds: (_, { biobanks }) => biobanks.map(b => b.id || b).filter(bid => bid !== undefined),
-  getCollectionsWithBiobankId: (state) => {
-    if (state.collectionInfo) {
-      return state.collectionInfo.map(colInfo => {
-        return {
-          collectionId: colInfo.collectionId,
-          biobankId: colInfo.biobankId
+    filterDefinitions,
+    bookmarkMappedToState: state => state.bookmarkMappedToState,
+    loading: ({ collectionInfo, biobankIds }) => !(biobankIds && collectionInfo),
+    biobanks: ({ collectionInfo, biobankIds, biobanks }, { loading, rsql }) => {
+        if (loading) {
+            return []
         }
-      })
-    }
-  },
-  foundBiobanks: (_, { biobanks }) => {
-    return biobanks.length
-  },
-  foundCollectionIds (_, { getFoundBiobankIds, getCollectionsWithBiobankId }) {
-    // only if there are biobanks, then there are collections. we can't have rogue collections :)
-    if (getFoundBiobankIds.length && getCollectionsWithBiobankId.length) {
-      const biobanksWithCollections = groupCollectionsByBiobankId(getCollectionsWithBiobankId)
-      let collectionIds = []
-      for (const id of getFoundBiobankIds) {
-        const collectionsInBiobank = biobanksWithCollections[id]
-        if (collectionsInBiobank) collectionIds = collectionIds.concat(collectionsInBiobank)
-      }
-      return collectionIds
-    }
-    return []
-  },
-  collectionsInPodium ({ podiumCollectionIds, collectionInfo, isPodium }, { foundCollectionIds }) {
-    if (isPodium && podiumCollectionIds && collectionInfo && foundCollectionIds) {
-      const collectionInfoInSelection = collectionInfo.filter(colInfo => foundCollectionIds.includes(colInfo.collectionId))
-      const collectionNames = collectionInfoInSelection.filter(colInfo => podiumCollectionIds
-        .includes(colInfo.collectionId))
-        .map(podCols => podCols.collectionName) // Returns only collection names.
-      return collectionNames
-    } else return []
-  },
-  selectedBiobankQuality: state => state.filters.selections.biobank_quality,
-  selectedCollectionQuality: state => {
-    return state.filters.selections.collection_quality
-  },
-  rsql: createRSQLQuery,
-  biobankRsql: createBiobankRSQLQuery,
-  resetPage: state => !state.isPaginating,
-  showCountryFacet: state => state.showCountryFacet,
-  /**
-   * Get map of active filters
-   */
-  getActiveFilters: state => state.filters.selections,
-  getErrorMessage: state => {
-    if (!state.error) {
-      return undefined
-    }
-    if (state.error.errors) {
-      return state.error.errors[0].message
-    }
-    if (state.error.message) {
-      return state.error.message
+        let ids = biobankIds
+        if (rsql && rsql.length) {
+            ids = collectionInfo
+                // biobank IDs present in collectionIds
+                .map(({ biobankId }) => biobankId)
+                // also present in biobankIds
+                .filter(biobankId => biobankIds.includes(biobankId))
+                // first occurrence of ID only
+                .filter((value, index, self) => self.indexOf(value) === index)
+        }
+        return ids.map(biobankId => {
+            if (!Object.prototype.hasOwnProperty.call(biobanks, biobankId)) {
+                return biobankId
+            }
+            const biobank = biobanks[biobankId]
+            return {
+                ...biobank,
+                collections: filterCollectionTree(collectionInfo.map(it => it.collectionId), biobank.collections)
+            }
+        })
+    },
+    getFoundBiobankIds: (_, { biobanks }) => biobanks.map(b => b.id || b).filter(bid => bid !== undefined),
+    getCollectionsWithBiobankId: (state) => {
+        if (state.collectionInfo) {
+            return state.collectionInfo.map(colInfo => {
+                return {
+                    collectionId: colInfo.collectionId,
+                    biobankId: colInfo.biobankId
+                }
+            })
+        }
+    },
+    foundBiobanks: (_, { biobanks }) => {
+        return biobanks.length
+    },
+    foundCollectionIds(_, { getFoundBiobankIds, getCollectionsWithBiobankId }) {
+        // only if there are biobanks, then there are collections. we can't have rogue collections :)
+        if (getFoundBiobankIds.length && getCollectionsWithBiobankId.length) {
+            const biobanksWithCollections = groupCollectionsByBiobankId(getCollectionsWithBiobankId)
+            let collectionIds = []
+            for (const id of getFoundBiobankIds) {
+                const collectionsInBiobank = biobanksWithCollections[id]
+                if (collectionsInBiobank) collectionIds = collectionIds.concat(collectionsInBiobank)
+            }
+            return collectionIds
+        }
+        return []
+    },
+    collectionsInPodium({ podiumCollectionIds, collectionInfo, isPodium }, { foundCollectionIds }) {
+        if (isPodium && podiumCollectionIds && collectionInfo && foundCollectionIds) {
+            const collectionInfoInSelection = collectionInfo.filter(colInfo => foundCollectionIds.includes(colInfo.collectionId))
+            const collectionNames = collectionInfoInSelection.filter(colInfo => podiumCollectionIds
+                    .includes(colInfo.collectionId))
+                .map(podCols => podCols.collectionName) // Returns only collection names.
+            return collectionNames
+        } else return []
+    },
+    selectedBiobankQuality: state => state.filters.selections.biobank_quality,
+    selectedCollectionQuality: state => {
+        return state.filters.selections.collection_quality
+    },
+    rsql: createRSQLQuery,
+    biobankRsql: createBiobankRSQLQuery,
+    resetPage: state => !state.isPaginating,
+    showCountryFacet: state => state.showCountryFacet,
+    /**
+     * Get map of active filters
+     */
+    getActiveFilters: state => state.filters.selections,
+    getErrorMessage: state => {
+        if (!state.error) {
+            return undefined
+        }
+        if (state.error.errors) {
+            return state.error.errors[0].message
+        }
+        if (state.error.message) {
+            return state.error.message
+        }
+        return 'Something went wrong'
     }
 }
