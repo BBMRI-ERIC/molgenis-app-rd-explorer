a0537a179f8a9f69e923dbc3407b62d7a1a91be1
Pacific89 <philipp.faulhammer@medunigraz.at>
2020-12-02 08:29:25 +0100

    remove some filters

--- src/utils/filterDefinitions.js
+++ src/utils/filterDefinitions.js
@@ -8,27 +8,6 @@ const filterDefinitions = (state) => [{
         humanReadableString: 'Text search is'
     },
     {
-        headerClass: 'bg-warning text-white',
-        component: 'CovidNetworkFilter',
-        name: 'covid19network',
-        label: 'COVID-19',
-        initiallyCollapsed: !state.route.query.collection_network || !state.route.query.biobank_network
-    },
-    {
-        headerClass: 'bg-warning text-white',
-        component: 'CovidFilter',
-        name: 'covid19',
-        label: 'COVID-19 Services',
-        type: 'checkbox-filter',
-        table: 'eu_bbmri_eric_COVID_19',
-        options: genericFilterOptions('eu_bbmri_eric_COVID_19'),
-        initiallyCollapsed: !state.route.query.covid19,
-        filters: state.filters.selections.covid19,
-        all: true,
-        maxVisibleOptions: 25,
-        humanReadableString: 'Covid-19 service(s):'
-    },
-    {
         component: 'MultiFilter',
         name: 'diagnosis_available',
         label: 'Diagnosis available',
@@ -39,6 +18,7 @@ const filterDefinitions = (state) => [{
         options: diagnosisAvailableFilterOptions('eu_bbmri_eric_disease_types'),
         initiallyCollapsed: !state.route.query.diagnosis_available,
         humanReadableString: 'Disease type(s):'
+        
     },
     {
         component: 'CheckboxFilter',
@@ -66,78 +46,6 @@ const filterDefinitions = (state) => [{
     },
     {
         component: 'CheckboxFilter',
-        name: 'biobank_quality',
-        label: 'Biobank quality marks',
-        type: 'checkbox-filter',
-        table: 'eu_bbmri_eric_assess_level_bio',
-        options: genericFilterOptions('eu_bbmri_eric_assess_level_bio'),
-        initiallyCollapsed: !state.route.query.biobank_quality,
-        filters: state.filters.selections.biobank_quality,
-        maxVisibleOptions: 25,
-        humanReadableString: 'Biobank quality mark(s):'
-    },
-    {
-        component: 'CheckboxFilter',
-        name: 'collection_quality',
-        label: 'Collection quality marks',
-        type: 'checkbox-filter',
-        table: 'eu_bbmri_eric_assess_level_col',
-        options: genericFilterOptions('eu_bbmri_eric_assess_level_col'),
-        initiallyCollapsed: !state.route.query.collection_quality,
-        filters: state.filters.selections.collection_quality,
-        maxVisibleOptions: 25,
-        humanReadableString: 'Collection quality mark(s):'
-    },
-    {
-        component: 'CheckboxFilter',
-        name: 'type',
-        label: 'Collection types',
-        type: 'checkbox-filter',
-        table: 'eu_bbmri_eric_collection_types',
-        options: genericFilterOptions('eu_bbmri_eric_collection_types'),
-        initiallyCollapsed: !state.route.query.type,
-        filters: state.filters.selections.type,
-        maxVisibleOptions: 25,
-        humanReadableString: 'Collection type(s):'
-    },
-    {
-        component: 'CheckboxFilter',
-        name: 'biobank_network',
-        label: 'Biobank network',
-        type: 'checkbox-filter',
-        table: 'eu_bbmri_eric_networks',
-        options: genericFilterOptions('eu_bbmri_eric_networks'),
-        initiallyCollapsed: !state.route.query.biobank_network,
-        filters: state.filters.selections.biobank_network,
-        maxVisibleOptions: 25,
-        humanReadableString: 'Biobank with network(s):'
-    },
-    {
-        component: 'CheckboxFilter',
-        name: 'collection_network',
-        label: 'Collection network',
-        type: 'checkbox-filter',
-        table: 'eu_bbmri_eric_networks',
-        options: genericFilterOptions('eu_bbmri_eric_networks'),
-        initiallyCollapsed: !state.route.query.collection_network,
-        filters: state.filters.selections.collection_network,
-        maxVisibleOptions: 25,
-        humanReadableString: 'Collection with network(s):'
-    },
-    {
-        component: 'CheckboxFilter',
-        name: 'dataType',
-        label: 'Data types',
-        type: 'checkbox-filter',
-        table: 'eu_bbmri_eric_data_types',
-        options: genericFilterOptions('eu_bbmri_eric_data_types'),
-        initiallyCollapsed: !state.route.query.dataType,
-        filters: state.filters.selections.dataType,
-        maxVisibleOptions: 25,
-        humanReadableString: 'Data type(s):'
-    },
-    {
-        component: 'CheckboxFilter',
         name: 'ressource_types',
         label: 'Ressource Types',
         type: 'checkbox-filter',
@@ -147,18 +55,6 @@ const filterDefinitions = (state) => [{
         filters: state.filters.selections.ressource_types,
         maxVisibleOptions: 25,
         humanReadableString: 'Ressource type(s):'
-    },
-    {
-        component: 'CheckboxFilter',
-        name: 'order_of_magnitude_donors',
-        label: 'Number Donors',
-        type: 'order_of_magnitude_donors',
-        table: 'eu_bbmri_eric_biobank_size',
-        options: genericFilterOptions('eu_bbmri_eric_biobank_size'),
-        initiallyCollapsed: !state.route.query.order_of_magnitude_donors,
-        filters: state.filters.selections.order_of_magnitude_donors,
-        maxVisibleOptions: 25,
-        humanReadableString: 'Donor number(s):'
     }
 ]
 
