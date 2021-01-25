3d08098af6b29e7aabefb5e40319981d8333a7e7
Pacific89 <philipp.faulhammer@medunigraz.at>
2021-01-25 08:52:34 +0100

    remove filters for tests

--- src/utils/filterDefinitions.js
+++ src/utils/filterDefinitions.js
@@ -1,5 +1,5 @@
 /* istanbul ignore file */
-import { genericFilterOptions, diagnosisAvailableFilterOptions, resscourceTypesAvailableFilterOptions } from './filterOptions'
+import { genericFilterOptions, diagnosisAvailableFilterOptions, resscourceTypesAvailableFilterOptions, newFilterFunc, newCountryFilterOption } from './filterOptions'
 
 const filterDefinitions = (state) => [
   {
@@ -74,7 +74,7 @@ const filterDefinitions = (state) => [
     label: 'Materials',
     type: 'checkbox-filter',
     table: 'eu_bbmri_eric_material_types',
-    options: genericFilterOptions('eu_bbmri_eric_material_types'),
+    options: newFilterFunc(),
     initiallyCollapsed: !state.route.query.materials,
     filters: state.filters.selections.materials,
     maxVisibleOptions: 25,
@@ -86,84 +86,84 @@ const filterDefinitions = (state) => [
     label: 'Countries',
     type: 'checkbox-filter',
     table: 'eu_bbmri_eric_countries',
-    options: genericFilterOptions('eu_bbmri_eric_countries'),
+    options: newCountryFilterOption(),
     initiallyCollapsed: !state.route.query.country,
     filters: state.filters.selections.country,
     maxVisibleOptions: 25,
     humanReadableString: 'Countries:'
-  },
-  {
-    component: 'CheckboxFilter',
-    name: 'biobank_quality',
-    label: 'Biobank quality marks',
-    type: 'checkbox-filter',
-    table: 'eu_bbmri_eric_assess_level_bio',
-    options: genericFilterOptions('eu_bbmri_eric_assess_level_bio'),
-    initiallyCollapsed: !state.route.query.biobank_quality,
-    filters: state.filters.selections.biobank_quality,
-    maxVisibleOptions: 25,
-    humanReadableString: 'Biobank quality mark(s):'
-  },
-  {
-    component: 'CheckboxFilter',
-    name: 'collection_quality',
-    label: 'Collection quality marks',
-    type: 'checkbox-filter',
-    table: 'eu_bbmri_eric_assess_level_col',
-    options: genericFilterOptions('eu_bbmri_eric_assess_level_col'),
-    initiallyCollapsed: !state.route.query.collection_quality,
-    filters: state.filters.selections.collection_quality,
-    maxVisibleOptions: 25,
-    humanReadableString: 'Collection quality mark(s):'
-  },
-  {
-    component: 'CheckboxFilter',
-    name: 'type',
-    label: 'Collection types',
-    type: 'checkbox-filter',
-    table: 'eu_bbmri_eric_collection_types',
-    options: genericFilterOptions('eu_bbmri_eric_collection_types'),
-    initiallyCollapsed: !state.route.query.type,
-    filters: state.filters.selections.type,
-    maxVisibleOptions: 25,
-    humanReadableString: 'Collection type(s):'
-  },
-  {
-    component: 'CheckboxFilter',
-    name: 'biobank_network',
-    label: 'Biobank network',
-    type: 'checkbox-filter',
-    table: 'eu_bbmri_eric_networks',
-    options: genericFilterOptions('eu_bbmri_eric_networks'),
-    initiallyCollapsed: !state.route.query.biobank_network,
-    filters: state.filters.selections.biobank_network,
-    maxVisibleOptions: 25,
-    humanReadableString: 'Biobank with network(s):'
-  },
-  {
-    component: 'CheckboxFilter',
-    name: 'collection_network',
-    label: 'Collection network',
-    type: 'checkbox-filter',
-    table: 'eu_bbmri_eric_networks',
-    options: genericFilterOptions('eu_bbmri_eric_networks'),
-    initiallyCollapsed: !state.route.query.collection_network,
-    filters: state.filters.selections.collection_network,
-    maxVisibleOptions: 25,
-    humanReadableString: 'Collection with network(s):'
-  },
-  {
-    component: 'CheckboxFilter',
-    name: 'dataType',
-    label: 'Data types',
-    type: 'checkbox-filter',
-    table: 'eu_bbmri_eric_data_types',
-    options: genericFilterOptions('eu_bbmri_eric_data_types'),
-    initiallyCollapsed: !state.route.query.dataType,
-    filters: state.filters.selections.dataType,
-    maxVisibleOptions: 25,
-    humanReadableString: 'Data type(s):'
   }
+  // {
+  //   component: 'CheckboxFilter',
+  //   name: 'biobank_quality',
+  //   label: 'Biobank quality marks',
+  //   type: 'checkbox-filter',
+  //   table: 'eu_bbmri_eric_assess_level_bio',
+  //   options: genericFilterOptions('eu_bbmri_eric_assess_level_bio'),
+  //   initiallyCollapsed: !state.route.query.biobank_quality,
+  //   filters: state.filters.selections.biobank_quality,
+  //   maxVisibleOptions: 25,
+  //   humanReadableString: 'Biobank quality mark(s):'
+  // },
+  // {
+  //   component: 'CheckboxFilter',
+  //   name: 'collection_quality',
+  //   label: 'Collection quality marks',
+  //   type: 'checkbox-filter',
+  //   table: 'eu_bbmri_eric_assess_level_col',
+  //   options: genericFilterOptions('eu_bbmri_eric_assess_level_col'),
+  //   initiallyCollapsed: !state.route.query.collection_quality,
+  //   filters: state.filters.selections.collection_quality,
+  //   maxVisibleOptions: 25,
+  //   humanReadableString: 'Collection quality mark(s):'
+  // },
+  // {
+  //   component: 'CheckboxFilter',
+  //   name: 'type',
+  //   label: 'Collection types',
+  //   type: 'checkbox-filter',
+  //   table: 'eu_bbmri_eric_collection_types',
+  //   options: genericFilterOptions('eu_bbmri_eric_collection_types'),
+  //   initiallyCollapsed: !state.route.query.type,
+  //   filters: state.filters.selections.type,
+  //   maxVisibleOptions: 25,
+  //   humanReadableString: 'Collection type(s):'
+  // },
+  // {
+  //   component: 'CheckboxFilter',
+  //   name: 'biobank_network',
+  //   label: 'Biobank network',
+  //   type: 'checkbox-filter',
+  //   table: 'eu_bbmri_eric_networks',
+  //   options: genericFilterOptions('eu_bbmri_eric_networks'),
+  //   initiallyCollapsed: !state.route.query.biobank_network,
+  //   filters: state.filters.selections.biobank_network,
+  //   maxVisibleOptions: 25,
+  //   humanReadableString: 'Biobank with network(s):'
+  // },
+  // {
+  //   component: 'CheckboxFilter',
+  //   name: 'collection_network',
+  //   label: 'Collection network',
+  //   type: 'checkbox-filter',
+  //   table: 'eu_bbmri_eric_networks',
+  //   options: genericFilterOptions('eu_bbmri_eric_networks'),
+  //   initiallyCollapsed: !state.route.query.collection_network,
+  //   filters: state.filters.selections.collection_network,
+  //   maxVisibleOptions: 25,
+  //   humanReadableString: 'Collection with network(s):'
+  // },
+  // {
+  //   component: 'CheckboxFilter',
+  //   name: 'dataType',
+  //   label: 'Data types',
+  //   type: 'checkbox-filter',
+  //   table: 'eu_bbmri_eric_data_types',
+  //   options: genericFilterOptions('eu_bbmri_eric_data_types'),
+  //   initiallyCollapsed: !state.route.query.dataType,
+  //   filters: state.filters.selections.dataType,
+  //   maxVisibleOptions: 25,
+  //   humanReadableString: 'Data type(s):'
+  // }
 ]
 
 export default filterDefinitions
